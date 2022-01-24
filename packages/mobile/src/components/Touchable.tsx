67c9f8a3be27373eb55277b4320a411302fe32e9
Rafael Canuto <rafaeloliveiracanuto@gmail.com>
2022-01-24 18:39:46 -0300

    Create mobile Touchable component

---
+++ packages/mobile/src/components/Touchable.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import { ComponentPropsWithoutRef, forwardRef } from 'react'
+import { ComponentVariants, useComponentStyle, BaseViewProps, ViewStyles } from '@codeleap/common'
+import { View } from '@codeleap/mobile/src'
+import { TouchableOpacity as NativeTouchable } from  'react-native'
+
+export type TouchableProps =
+  ComponentPropsWithoutRef<typeof NativeTouchable> & 
+{
+  variants?: ComponentVariants <typeof ViewStyles>['variants'],
+} & BaseViewProps
+
+export const Touchable = forwardRef<NativeTouchable, TouchableProps>((touchableProps, ref) => {
+  const { 
+    variants = [],
+    children,
+    onPress,
+    style,
+    ...props
+  } = touchableProps
+  
+  const variantStyles = useComponentStyle('View', {
+    variants,
+  })
+
+  const press = () => {
+    console.log('<Touchable/> pressed', touchableProps)
+    onPress(null)
+  }
+  
+  const styles = [variantStyles.wrapper, style]
+
+  return <NativeTouchable onPress={press} style={styles} ref={ref}> 
+    <View {...props}>
+      {children}
+    </View>
+  </NativeTouchable>
+})
