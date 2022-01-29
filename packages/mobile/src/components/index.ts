2610cd9592998f89e39db4d719600a582567f8dd
Rafael Canuto <rafaeloliveiracanuto@gmail.com>
2022-01-29 11:56:37 -0300

    Export Touchable, Scroll and ActivityIndicator in index

--- packages/mobile/src/components/index.ts
+++ packages/mobile/src/components/index.ts
@@ -1,3 +1,5 @@
 export { Text } from './Text'
 export { View } from './View'
 export { Touchable } from './Touchable'
+export { Scroll } from './Scroll'
+export { ActivityIndicator } from './ActivityIndicator'
