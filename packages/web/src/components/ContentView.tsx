74c8d9c7b63d5fc4c57be67837e5e87e9ba32fea
Paulo Henrique De Souza <paulosouza300272@gmail.com>
2022-01-31 21:31:47 -0300

    re add CONTENTViEW

---
+++ packages/web/src/components/ContentView.tsx
@@ -0,0 +1,48 @@
+import { ComponentVariants, ContentViewStyles,  useComponentStyle, ViewComposition } from '@codeleap/common'
+import { ActivityIndicator } from '.'
+import { ViewProps, View } from './View'
+import { Text } from './Text'
+import { StylesOf } from '../types/utility'
+
+export type ContentViewProps = Omit<ViewProps<'div'>, 'variants' | 'responsiveVariants'> & {
+    placeholderMsg: string
+    loading?:boolean
+    styles?: StylesOf<ViewComposition>
+} & ComponentVariants<typeof ContentViewStyles>
+
+const WrapContent = ({children,  ...props}:Partial<ContentViewProps>) => <View {...props} >{children}</View>
+
+export const ContentView:React.FC<ContentViewProps> = (rawProps) => {
+  const {
+    children,
+    placeholderMsg,
+    loading,
+    variants,
+    responsiveVariants,
+    styles,
+    ...props
+  } = rawProps
+
+  const variantStyle = useComponentStyle('ContentView', {
+    variants,
+    responsiveVariants,
+    styles,
+  })
+    
+  if (loading){
+    return <WrapContent {...props} css={{...variantStyle.wrapper}}>
+      <ActivityIndicator css={variantStyle.loader}/>
+    </WrapContent>
+  }
+  const hasChildren = Object.keys(children||{}).length > 0
+  if (hasChildren){
+    return <WrapContent {...props}css={variantStyle.wrapper}>
+      {children}
+    </WrapContent>
+  }
+
+  return <WrapContent {...props} css={variantStyle.wrapper}>
+    <Text text={placeholderMsg} css={variantStyle.placeholder}/>
+  </WrapContent>
+    
+}
