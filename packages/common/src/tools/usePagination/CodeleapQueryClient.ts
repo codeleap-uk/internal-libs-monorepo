266116cba0107d597460473257f9c7e8cf4de608
PSouza-10 <paulosouza300272@gmail.com>
2022-12-12 16:31:22 -0300

    feat(common): add setData method to QueryKey Client

--- packages/common/src/tools/usePagination/CodeleapQueryClient.ts
+++ packages/common/src/tools/usePagination/CodeleapQueryClient.ts
@@ -1,29 +1,41 @@
 import * as ReactQuery from '@tanstack/react-query'
+import { TypeGuards } from '../../utils'
+type Updater<Prev, Next> = Next | ((prev: Prev) => Next)
 
 type StaticQueryKeyClient<Data = any> = {
     key: ReactQuery.QueryKey
     getData(): Data
     refresh(): void
+    setData(updater: Updater<Data, Data>): Data
   }
 type QueryKeyBuilder<Args extends any[] = any[]> = (...args:Args) => ReactQuery.QueryKey
 
 type DynamicQueryKeyClient<Data = any, BuilderParams extends any[] = any[]> = {
     key(...args: BuilderParams): ReactQuery.QueryKey
     getData(...args: BuilderParams): Data
+    setData(keyParams: BuilderParams, updater: Updater<Data, Data>): Data
     refresh(...args: BuilderParams): void
   }
-
 export class CodeleapQueryClient extends ReactQuery.QueryClient {
   dynamicQueryKey<Data, BuilderArgs extends any[] = any[]>(k: QueryKeyBuilder<BuilderArgs>):DynamicQueryKeyClient<Data, BuilderArgs> {
 
     return {
       key: k,
-      getData: (...params:BuilderArgs) => {
+      getData: (...params) => {
 
         return this.getQueryData<Data>(k(...params))
       },
-      refresh: (...params:BuilderArgs) => {
-        this.invalidateQueries({
+      setData: (params, updater) => {
+        const _updater = (prev:Data) => {
+          if (TypeGuards.isFunction(updater)) {
+            return updater(prev)
+          }
+          return updater
+        }
+        return this.setQueryData(k(...params), _updater)
+      },
+      refresh: (...params) => {
+        return this.invalidateQueries({
           exact: true,
           queryKey: k(...params),
           refetchPage: () => true,
@@ -39,8 +51,17 @@ export class CodeleapQueryClient extends ReactQuery.QueryClient {
       getData: () => {
         return this.getQueryData<Data>(k)
       },
+      setData: (updater) => {
+        const _updater = (prev:Data) => {
+          if (TypeGuards.isFunction(updater)) {
+            return updater(prev)
+          }
+          return updater
+        }
+        return this.setQueryData(k, _updater)
+      },
       refresh: () => {
-        this.invalidateQueries({
+        return this.invalidateQueries({
           exact: true,
           queryKey: k,
           refetchPage: () => true,
