197cf348eb07346a26bf96457c65fcf1cf930e6d
Fernando Chaves <fernando.chaves16@hotmail.com>
2023-04-26 13:48:57 -0300

    chore: adding multiselect and autocomplete structure

---
+++ packages/web/src/components/MultiSelect/index.tsx
@@ -0,0 +1,186 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/react'
+
+import {
+  AnyFunction,
+  ComponentVariants,
+  IconPlaceholder,
+  onUpdate,
+  useDefaultComponentStyle,
+} from '@codeleap/common'
+
+import { ReactNode, useEffect, useId, useLayoutEffect, useRef } from 'react'
+import ReactDOM from 'react-dom'
+
+import { v4 } from 'uuid'
+
+import { StylesOf } from '../../types/utility'
+import { Button } from '../Button'
+import { View } from '../View'
+import { Text } from '../Text'
+import { Overlay } from '../Overlay'
+
+import { ModalComposition, ModalPresets } from './styles'
+
+export * from './styles'
+
+export type MultiSelectProps = React.PropsWithChildren<{
+  visible: boolean
+  title?: React.ReactNode
+  toggle: AnyFunction
+  styles?: StylesOf<ModalComposition>
+  accessible?: boolean
+  showClose?: boolean
+  closable?: boolean
+  scroll?: boolean
+  footer?: ReactNode
+  debugName?: string
+} & ComponentVariants<typeof ModalPresets>
+>
+function focusModal(event: FocusEvent, id: string) {
+  event.preventDefault()
+  const modal = document.getElementById(id)
+  if (modal) {
+    modal.focus()
+  }
+}
+export const ModalContent: React.FC<MultiSelectProps & { id: string }> = (
+  modalProps,
+) => {
+  const {
+    children,
+    closable = true,
+    visible,
+    title = '',
+    toggle,
+    responsiveVariants,
+    variants,
+    styles,
+    showClose = true,
+    footer,
+    ...props
+  } = modalProps
+
+  const id = useId()
+
+  const variantStyles = useDefaultComponentStyle('u:Modal', {
+    responsiveVariants,
+    variants,
+    styles,
+  })
+
+  onUpdate(() => {
+    if (visible) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+
+    }
+  }, [visible])
+
+  function closeOnEscPress(e: React.KeyboardEvent<HTMLDivElement>) {
+    if (e.key === 'Escape') {
+      toggle()
+    }
+  }
+
+  useLayoutEffect(() => {
+    const modal = document.getElementById(id)
+    if (modal) {
+      modal.focus()
+    }
+  }, [id])
+
+  return (
+    <View
+      aria-hidden={!visible}
+      css={variantStyles.wrapper}
+      className={visible ? 'visible' : ''}
+    >
+      <Overlay
+        visible={visible}
+        onPress={closable ? toggle : () => {}}
+        css={variantStyles.overlay}
+      />
+      <View
+        component='section'
+        css={{
+          ...variantStyles.box,
+          // visibility: visible ? 'visible' : 'hidden',
+        }}
+        className='content'
+        onKeyDown={closeOnEscPress}
+        tabIndex={0}
+        id={id}
+        aria-modal={true}
+        role='dialog'
+        aria-describedby={`${id}-title`}
+        aria-label='Close the modal by presing Escape key'
+        {...props}
+      >
+        {(title || showClose) && (
+          <View
+            component='header'
+            className='modal-header header'
+            id={`${id}-title`}
+            css={variantStyles.header}
+          >
+            {typeof title === 'string' ? <Text text={title} css={variantStyles.title} /> : title}
+
+            {showClose && closable && (
+              <Button
+                rightIcon={'close' as IconPlaceholder}
+                variants={['icon']}
+                onPress={toggle}
+                css={variantStyles.closeButton}
+              />
+            )}
+          </View>
+        )}
+
+        <View css={variantStyles.body}>{children}</View>
+        {footer && (
+          <View component='footer' css={variantStyles.footer}>
+            {footer}
+          </View>
+        )}
+      </View>
+    </View>
+  )
+}
+
+export const MultiSelect: React.FC<MultiSelectProps> = ({ accessible, ...props }) => {
+  const modalId = useRef(v4())
+
+  useEffect(() => {
+    if (accessible) {
+      const currentId = modalId.current
+      const appRoot = document.body
+      appRoot.addEventListener('focusin', (e) => focusModal(e, currentId))
+      return () => appRoot.removeEventListener('focusin', (e) => focusModal(e, currentId))
+    }
+  }, [])
+
+  useEffect(() => {
+    if (accessible) {
+      const appRoot = document.body
+      appRoot.setAttribute('aria-hidden', `${props.visible}`)
+      appRoot.setAttribute('tabindex', `${-1}`)
+    }
+  }, [props.visible])
+
+  if (accessible) {
+    if (props.visible) {
+      document.body.style.overflow = 'hidden'
+      return ReactDOM.createPortal(
+        <ModalContent {...props} id={modalId.current} />,
+        document.body,
+      )
+    } else {
+      document.body.style.overflow = 'visible'
+      return null
+    }
+  }
+
+  return <ModalContent {...props} id={modalId.current} />
+}
--- packages/web/src/components/defaultStyles.ts
+++ packages/web/src/components/defaultStyles.ts
@@ -1,21 +1,21 @@
-import {ViewPresets} from './View/styles'
-import {IconPresets} from './Icon/styles'
-import {TouchablePresets} from './Touchable/styles'
-import {TextPresets} from './Text/styles'
-import {ScrollPresets} from './Scroll/styles'
-import {ActivityIndicatorPresets} from './ActivityIndicator/styles'
-import {ButtonPresets} from './Button/styles'
-import {ActionIconPresets} from './ActionIcon/styles'
-import {ModalPresets} from './Modal/styles'
-import {CheckboxPresets} from './Checkbox/styles'
-import {CollapsePresets} from './Collapse/styles'
-import {DrawerPresets} from './Drawer/styles'
-import {OverlayPresets} from './Overlay/styles'
-import {TextInputPresets} from './TextInput/styles'
-import {RadioInputPresets} from './RadioInput/styles'
-import {SelectPresets} from './Select/styles'
-import {TooltipPresets} from './Tooltip/styles'
-import {ListPresets} from './List/styles'
+import { ViewPresets } from './View/styles'
+import { IconPresets } from './Icon/styles'
+import { TouchablePresets } from './Touchable/styles'
+import { TextPresets } from './Text/styles'
+import { ScrollPresets } from './Scroll/styles'
+import { ActivityIndicatorPresets } from './ActivityIndicator/styles'
+import { ButtonPresets } from './Button/styles'
+import { ActionIconPresets } from './ActionIcon/styles'
+import { ModalPresets } from './Modal/styles'
+import { CheckboxPresets } from './Checkbox/styles'
+import { CollapsePresets } from './Collapse/styles'
+import { DrawerPresets } from './Drawer/styles'
+import { OverlayPresets } from './Overlay/styles'
+import { TextInputPresets } from './TextInput/styles'
+import { RadioInputPresets } from './RadioInput/styles'
+import { SelectPresets } from './Select/styles'
+import { TooltipPresets } from './Tooltip/styles'
+import { ListPresets } from './List/styles'
 import { LinkPresets } from './Link/styles'
 import { SliderPresets } from './Slider/styles'
 import { LoadingOverlayPresets } from './LoadingOverlay/styles'
@@ -25,6 +25,7 @@ export const defaultStyles = {
   Icon: IconPresets,
   Touchable: TouchablePresets,
   Text: TextPresets,
+  AutoComplete: AutoCompletePresets,
   ActivityIndicator: ActivityIndicatorPresets,
   ActionIcon: ActionIconPresets,
   Scroll: ScrollPresets,
@@ -41,6 +42,6 @@ export const defaultStyles = {
   Tooltip: TooltipPresets,
   List: ListPresets,
   Slider: SliderPresets,
-  MultiSelect:SelectPresets,
+  MultiSelect: SelectPresets,
   LoadingOverlay: LoadingOverlayPresets,
 }
