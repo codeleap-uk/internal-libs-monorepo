e8421ee57044c60a4181d196a9c95b511684eeb9
leonardo-weber <weberleonardo2002@gmail.com>
2023-05-02 09:09:38 -0300

    WIP: changes on date picker modal packages style file

--- packages/mobile/src/components/DatePickerModal/styles.ts
+++ packages/mobile/src/components/DatePickerModal/styles.ts
@@ -3,19 +3,4 @@ import { ButtonComposition, createDefaultVariantFactory, includePresets } from '
 export type DatePickerModalComposition = 'labelText' | `button${Capitalize<ButtonComposition>}`
 
 const createDatePickerModalStyle = createDefaultVariantFactory<DatePickerModalComposition>()
-
 export const DatePickerModalPresets = includePresets((styles) => createDatePickerModalStyle(() => ({ buttonWrapper: styles })))
-
-export const DatePickerModalStyles = {
-  ...DatePickerModalPresets,
-  default: createDatePickerModalStyle((theme) => ({
-    buttonWrapper: {
-      backgroundColor: theme.colors.lightGray,
-      borderRadius: theme.borderRadius.medium,
-      justifyContent: 'space-between',
-    },
-    labelText: {
-
-    },
-  })),
-}
