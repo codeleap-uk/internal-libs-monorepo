13bf937da37ae2859cfb3b1ef2988930962df88d
leonardo-weber <weberleonardo2002@gmail.com>
2023-05-26 11:20:07 -0300

    using value or new date instead of null

--- packages/mobile/src/components/DatePickerModal/index.tsx
+++ packages/mobile/src/components/DatePickerModal/index.tsx
@@ -132,7 +132,7 @@ export const DatePickerModal = (props: DatePickerModalProps) => {
   })
 
   const [visible, toggle] = !TypeGuards.isNil(_visible) && !!_toggle ? [_visible, _toggle] : useBooleanToggle(false)
-  const [value, setValue] = _value && onValueChange ? [_value, onValueChange] : useState(null)
+  const [value, setValue] = _value && onValueChange ? [_value, onValueChange] : useState(_value ?? new Date())
 
   const Wrapper = isCustomModal ? ModalManager.Modal : React.Fragment
 
