79fbaa04473ab6ed0bd45251ab89de0d1492b8cf
Luiz Felipe <felipefelizatti215@gmail.com>
2023-05-30 20:56:27 -0300

    chore(web): select styles refactor

--- packages/web/src/components/Select/styles.ts
+++ packages/web/src/components/Select/styles.ts
@@ -1,5 +1,5 @@
 import { createDefaultVariantFactory, FormTypes, includePresets, useDefaultComponentStyle } from '@codeleap/common'
-import { GroupBase, StylesConfig } from 'react-select'
+import { CSSObjectWithLabel, GroupBase, StylesConfig } from 'react-select'
 import { InputBaseParts } from '../InputBase'
 import { SelectProps } from './types'
 
@@ -7,6 +7,8 @@ export type SelectParts =
   InputBaseParts
   | 'container'
   | 'control'
+  | 'group'
+  | 'groupHeading'
   | 'listWrapper'
   | 'listWrapper'
   | 'list'
@@ -63,137 +65,37 @@ export function useSelectStyles<T, Multi extends boolean>(props: SelectProps<T, 
     },
   )
 
+  const stylesKey = (key: SelectParts, styles: CSSObjectWithLabel = {}) => ({
+    ...styles,
+    ...variantStyles[key],
+    ...(focused ? variantStyles[key + ':focused'] : {}),
+    ...(disabled ? variantStyles[key + ':disabled'] : {}),
+    ...(error ? variantStyles[key + ':error'] : {}),
+  })
+
   const reactSelectStyles: StylesConfig<FormTypes.Option<T>, Multi, GroupBase<FormTypes.Option<T>>> = {
-    container: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.container,
-      ...(focused ? variantStyles['container:focused'] : {}),
-      ...(disabled ? variantStyles['container:disabled'] : {}),
-      ...(error ? variantStyles['container:error'] : {}),
-    }),
-    control: (baseStyles, state) => ({
-      ...variantStyles.container,
-      ...(focused ? variantStyles['control:focused'] : {}),
-      ...(disabled ? variantStyles['control:disabled'] : {}),
-      ...(error ? variantStyles['control:error'] : {}),
-    }),
-    menuPortal: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.listPortal,
-      ...(error ? variantStyles['listPortal:error'] : {}),
-    }),
-    menu: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.listWrapper,
-      ...(error ? variantStyles['listWrapper:error'] : {}),
-    }),
-    menuList: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.list,
-      ...(error ? variantStyles['list:error'] : {}),
-    }),
-    group: (baseStyles, state) => ({
-      ...baseStyles,
-      overflow: 'visible',
-      ...(error ? variantStyles['group:error'] : {}),
-    }),
-    indicatorSeparator: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.separatorIcon,
-      ...(focused ? variantStyles['separatorIcon:focused'] : {}),
-      ...(disabled ? variantStyles['separatorIcon:disabled'] : {}),
-      ...(error ? variantStyles['separatorIcon:error'] : {}),
-    }),
-    groupHeading: (baseStyles, state) => ({
-      ...baseStyles,
-      ...(error ? variantStyles['groupHeading:error'] : {}),
-    }),
-    clearIndicator: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.clearIcon,
-      ...(focused ? variantStyles['clearIcon:focused'] : {}),
-      ...(error ? variantStyles['clearIcon:error'] : {}),
-    }),
-    dropdownIndicator: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.dropdownIcon,
-      ...(focused ? variantStyles['dropdownIcon:focused'] : {}),
-      ...(disabled ? variantStyles['dropdownIcon:disabled'] : {}),
-      ...(error ? variantStyles['dropdownIcon:error'] : {}),
-    }),
-    indicatorsContainer: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.iconsWrapper,
-      ...(disabled ? variantStyles['iconsWrapper:disabled'] : {}),
-      ...(error ? variantStyles['iconsWrapper:error'] : {}),
-    }),
-    input: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.input,
-      ...(disabled ? variantStyles['input:disabled'] : {}),
-      ...(error ? variantStyles['input:error'] : {}),
-    }),
-    loadingIndicator: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.loadingIcon,
-      ...(focused ? variantStyles['loadingIndicator:focused'] : {}),
-      ...(disabled ? variantStyles['loadingIndicator:disabled'] : {}),
-      ...(error ? variantStyles['loadingIndicator:error'] : {}),
-    }),
-    loadingMessage: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.loadingText,
-      ...(error ? variantStyles['loadingText:error'] : {}),
-    }),
-    multiValue: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.inputMultiValue,
-      ...(focused ? variantStyles['inputMultiValue:focused'] : {}),
-      ...(disabled ? variantStyles['inputMultiValue:disabled'] : {}),
-      ...(error ? variantStyles['inputMultiValue:error'] : {}),
-    }),
-    multiValueLabel: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.inputMultiValueLabel,
-      ...(focused ? variantStyles['inputMultiValueLabel:focused'] : {}),
-      ...(disabled ? variantStyles['inputMultiValueLabel:disabled'] : {}),
-      ...(error ? variantStyles['inputMultiValueLabel:error'] : {}),
-    }),
-    multiValueRemove: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.inputMultiValueRemove,
-    }),
-    noOptionsMessage: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.noItems,
-      ...(error ? variantStyles['noItems:error'] : {}),
-    }),
-    option: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.item,
-      ...(focused ? variantStyles['item:focused'] : {}),
-      ...(disabled ? variantStyles['item:disabled'] : {}),
-      ...(error ? variantStyles['item:error'] : {}),
-    }),
-    placeholder: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.placeholder,
-      ...(focused ? variantStyles['placeholder:focused'] : {}),
-      ...(disabled ? variantStyles['placeholder:disabled'] : {}),
-      ...(error ? variantStyles['placeholder:error'] : {}),
-    }),
-    singleValue: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.inputValue,
-      ...(disabled ? variantStyles['inputValue:disabled'] : {}),
-      ...(error ? variantStyles['inputValue:error'] : {}),
-    }),
-    valueContainer: (baseStyles, state) => ({
-      ...baseStyles,
-      ...variantStyles.inputValueWrapper,
-      ...(disabled ? variantStyles['inputValueWrapper:disabled'] : {}),
-      ...(error ? variantStyles['inputValueWrapper:error'] : {}),
-    }),
+    container: (baseStyles) => stylesKey('container', baseStyles),
+    control: () => stylesKey('container'),
+    menuPortal: (baseStyles) => stylesKey('listPortal', baseStyles),
+    menu: (baseStyles) => stylesKey('listWrapper', baseStyles),
+    menuList: (baseStyles) => stylesKey('list', baseStyles),
+    group: (baseStyles) => stylesKey('group', baseStyles),
+    indicatorSeparator: (baseStyles) => stylesKey('separatorIcon', baseStyles),
+    groupHeading: (baseStyles) => stylesKey('groupHeading', baseStyles),
+    clearIndicator: (baseStyles) => stylesKey('clearIcon', baseStyles),
+    dropdownIndicator: (baseStyles) => stylesKey('dropdownIcon', baseStyles),
+    indicatorsContainer: (baseStyles) => stylesKey('iconsWrapper', baseStyles),
+    input: (baseStyles) => stylesKey('input', baseStyles),
+    loadingIndicator: (baseStyles) => stylesKey('loadingIcon', baseStyles),
+    loadingMessage: (baseStyles) => stylesKey('loadingText', baseStyles),
+    multiValue: (baseStyles) => stylesKey('inputMultiValue', baseStyles),
+    multiValueLabel: (baseStyles) => stylesKey('inputMultiValueLabel', baseStyles),
+    multiValueRemove: (baseStyles) => stylesKey('inputMultiValueRemove', baseStyles),
+    noOptionsMessage: (baseStyles) => stylesKey('noItems', baseStyles),
+    option: (baseStyles) => stylesKey('item', baseStyles),
+    placeholder: (baseStyles) => stylesKey('placeholder', baseStyles),
+    singleValue: (baseStyles) => stylesKey('inputValue', baseStyles),
+    valueContainer: (baseStyles) => stylesKey('inputValueWrapper', baseStyles),
   }
 
   return {
