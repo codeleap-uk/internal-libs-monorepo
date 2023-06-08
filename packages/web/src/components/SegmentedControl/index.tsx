2b2100accffafc4b00b0f569a53e78618cd38151
Fernando Chaves <fernando.chaves16@hotmail.com>
2023-06-08 10:56:03 -0300

    chore: update

--- packages/web/src/components/SegmentedControl/index.tsx
+++ packages/web/src/components/SegmentedControl/index.tsx
@@ -1,5 +1,16 @@
 import { View } from '../View'
 
+export interface SegmentedControlProps {
+  visible?: boolean
+  /**
+   * Determine if the modal is visible
+  */
+  title?: React.ReactNode
+  /**
+    * Determine if the modal is visible
+  */
+}
+
 export const SegmentedControl = () => {
   return (
     <View />
