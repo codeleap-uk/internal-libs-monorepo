9799f632cf2d3462b32e3fdf40d12b56c232eb24
Luiz Felipe <felipefelizatti215@gmail.com>
2023-06-24 15:23:06 -0300

    chore(web): touchable improvements

--- packages/web/src/components/Touchable/index.tsx
+++ packages/web/src/components/Touchable/index.tsx
@@ -1,26 +1,26 @@
-/** @jsx jsx */
-import { AnyFunction, useCodeleapContext } from '@codeleap/common'
-import { jsx, CSSObject } from '@emotion/react'
-
+import { AnyFunction, ComponentVariants, onMount, TypeGuards, useCodeleapContext, useDefaultComponentStyle } from '@codeleap/common'
 import React, { ComponentPropsWithRef, ElementType, forwardRef, ReactElement } from 'react'
 import { stopPropagation } from '../../lib'
 import { View } from '../View'
-import { TouchableComposition } from './styles'
+import { TouchableComposition, TouchablePresets } from './styles'
 import { StylesOf } from '../../types'
-
+import { CSSInterpolation } from '@emotion/css'
 
 export * from './styles'
 
 export type TouchableProps<T extends ElementType = 'button'> = ComponentPropsWithRef<T> & {
-  css?: CSSObject
+  css?: CSSInterpolation | CSSInterpolation[]
   component?: T
   disabled?: boolean
   propagate?: boolean
   onPress?: AnyFunction
-  debugComponent?: string
   debugName?: string
+  debugComponent?: string
   styles?: StylesOf<TouchableComposition>
-}
+  debounce?: number
+  leadingDebounce?: boolean
+  setPressed?: (param: boolean) => void
+} & ComponentVariants<typeof TouchablePresets>
 
 export const TouchableCP = <T extends ElementType = typeof View>(
   touchableProps: TouchableProps<T>,
@@ -29,39 +29,91 @@ export const TouchableCP = <T extends ElementType = typeof View>(
   const {
     children,
     propagate = true,
+    debounce = null,
+    leadingDebounce,
+    setPressed,
     component: Component = View,
     disabled,
     onPress,
     onClick,
-    debugComponent,
     debugName,
+    debugComponent,
+    style = {},
+    styles = {},
+    responsiveVariants = {},
+    variants = [],
+    css = [],
     ...props
-  } = touchableProps
+  } = touchableProps as TouchableProps
+
+  const pressed = React.useRef(!!leadingDebounce)
+
+  onMount(() => {
+    if (!!leadingDebounce && !!debounce) {
+      setTimeout(() => {
+        pressed.current = false
+      }, debounce)
+    }
+  })
+
+  const variantStyles = useDefaultComponentStyle<'u:Touchable', typeof TouchablePresets>('u:Touchable', {
+    responsiveVariants,
+    variants,
+    styles,
+    rootElement: 'wrapper'
+  })
 
   const { logger } = useCodeleapContext()
 
-  const handleClick = (event: React.MouseEvent<T>) => {
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     if (disabled) return
 
     if (!propagate) stopPropagation(event)
 
-    if (!onPress) { 
-      logger.warn('No onPress passed to touchable', touchableProps)
+    if (!TypeGuards.isFunction(onPress)) { 
+      logger.warn(
+        'No onPress passed to touchable', 
+        touchableProps, 
+        'User interaction'
+      )
       return
-     }
+    }
 
-    logger.log(
-      `<${debugComponent || 'Touchable'}/>  pressed`,
-      { debugName, debugComponent },
-      'User interaction',
-    )
-    onClick?.()
-    onPress && onPress()
-    // logger.log('Touchable pressed', JSON.stringify(touchableProps, null, 2)  ,'Component')
+    const _onPress = () => {
+      logger.log(
+        `<${debugComponent || 'Touchable'}/> pressed`, 
+        { debugName }, 
+        'User interaction'
+      )
+      
+      if(TypeGuards.isFunction(onClick)) onClick?.(event)
+      onPress?.()
+    }
+
+    if (TypeGuards.isNumber(debounce)) {
+      if (pressed.current) {
+        return
+      }
+      setPressed?.(true)
+      pressed.current = true
+      _onPress()
+      setTimeout(() => {
+        setPressed?.(false)
+        pressed.current = false
+      }, debounce)
+    } else {
+      _onPress()
+    }
   }
 
   return (
-    <View component={Component || 'div'} {...props} onClick={handleClick} ref={ref}>
+    <View
+      component={Component || 'div'} 
+      {...props} 
+      onClick={handleClick} 
+      ref={ref}
+      css={[variantStyles.wrapper, css, style]}
+    >
       {children}
     </View>
   )
--- packages/web/src/components/Touchable/styles.ts
+++ packages/web/src/components/Touchable/styles.ts
@@ -1,11 +1,7 @@
 import { createDefaultVariantFactory, includePresets } from '@codeleap/common'
-import { StylesOf } from '../../types'
 
 export type TouchableComposition = 'wrapper'
 
-const createTouchableStyle = createDefaultVariantFactory<
-  TouchableComposition
->()
+const createTouchableStyle = createDefaultVariantFactory<TouchableComposition>()
 
 export const TouchablePresets = includePresets((styles) => createTouchableStyle(() => ({ wrapper: styles })))
-
