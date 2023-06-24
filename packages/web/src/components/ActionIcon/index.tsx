4380662752e6ab885a28873836257e74000bf0ab
Luiz Felipe <felipefelizatti215@gmail.com>
2023-06-24 18:06:05 -0300

    chore(web): action icon improvements

--- packages/web/src/components/ActionIcon/index.tsx
+++ packages/web/src/components/ActionIcon/index.tsx
@@ -4,20 +4,43 @@ import { StylesOf } from '../../types'
 import { Icon, IconProps } from '../Icon'
 import { Touchable, TouchableProps } from '../Touchable'
 import { View } from '../View'
-import { ActionIconComposition, ActionIconPresets } from './styles'
+import { ActionIconComposition, ActionIconParts, ActionIconPresets } from './styles'
 
-export type ActionIconProps = {
-    disabled?: boolean
-    iconProps?: Partial<IconProps>
-    icon?: IconProps['name']
-    name?: IconProps['name']
-    styles?: StylesOf<ActionIconComposition>
-} & Omit<TouchableProps, 'styles' | 'variants'> & ComponentVariants<typeof ActionIconPresets>
+export * from './styles'
+
+export type ActionIconProps = Omit<TouchableProps, 'styles' | 'variants'> & {
+  disabled?: boolean
+  iconProps?: Partial<IconProps>
+  icon?: IconProps['name']
+  name?: IconProps['name']
+  styles?: StylesOf<ActionIconComposition>
+} & ComponentVariants<typeof ActionIconPresets>
+
+const defaultProps: Partial<ActionIconProps> = {
+  disabled: false,
+}
 
 export const ActionIcon:React.FC<ActionIconProps> = (props) => {
-  const { icon, name, iconProps, onPress, variants, styles, children, disabled, ...touchableProps } = props
+  const allProps = {
+    ...ActionIcon.defaultProps,
+    ...props,
+  }
+
+  const { 
+    icon,
+    name,
+    iconProps,
+    onPress,
+    responsiveVariants = {},
+    variants = [],
+    styles = {},
+    children,
+    disabled,
+    ...touchableProps 
+  } = allProps
   
   const variantStyles = useDefaultComponentStyle<'u:ActionIcon', typeof ActionIconPresets>('u:ActionIcon', {
+    responsiveVariants,
     variants, 
     styles
   })
@@ -31,24 +54,23 @@ export const ActionIcon:React.FC<ActionIconProps> = (props) => {
 
     if (onPress) onPress?.()
   }
-  
+
+  const getStyles = (key: ActionIconParts) => ({
+    ...variantStyles[key],
+    ...(disabled && variantStyles[`${key}:disabled`]),
+    ...(isPressable && variantStyles[`${key}:pressable`]),
+  })
+
   return (
     <WrapperComponent 
       onPress={handlePress}
-      css={[
-        variantStyles.wrapper,
-        disabled && variantStyles['wrapper:disabled'],
-        isPressable && variantStyles['wrapper:cursor']
-      ]}
+      style={getStyles('touchableWrapper')}
       disabled={disabled}
       {...touchableProps}
     >
-      <Icon 
-        name={icon ?? name} 
-        css={[
-          variantStyles.icon,
-          disabled && variantStyles['icon:disabled'],
-        ]}
+      <Icon
+        name={icon ?? name}
+        style={getStyles('icon')}
         {...iconProps}
       />
       {children}
@@ -56,4 +78,4 @@ export const ActionIcon:React.FC<ActionIconProps> = (props) => {
   )
 }
 
-export * from './styles'
+ActionIcon.defaultProps = defaultProps
--- packages/web/src/components/ActionIcon/styles.ts
+++ packages/web/src/components/ActionIcon/styles.ts
@@ -1,9 +1,12 @@
 import { createDefaultVariantFactory, includePresets } from '@codeleap/common'
-import { TouchableComposition } from '../Touchable'
+import { IconComposition } from '../Icon'
+import { TouchableParts } from '../Touchable'
 
-export type ActionIconParts = 'icon' | TouchableComposition
-export type ActionIconStates = ':disabled' | ':cursor' | ''
-export type ActionIconComposition = `${ActionIconParts}${ActionIconStates}`
+export type ActionIconParts = IconComposition | `touchable${Capitalize<TouchableParts>}`
+export type ActionIconStates = 'disabled' | 'pressable'
+
+export type ActionIconComposition = ActionIconParts | `${ActionIconParts}:${ActionIconStates}`
+
 const createActionIconStyle = createDefaultVariantFactory<ActionIconComposition>()
 
-export const ActionIconPresets = includePresets((style) => createActionIconStyle(() => ({ wrapper: style })))
+export const ActionIconPresets = includePresets((style) => createActionIconStyle(() => ({ touchableWrapper: style })))
