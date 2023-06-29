9f351941c1f2c001ddd5e4ebeb6afa59d951a37c
Luiz Felipe <felipefelizatti215@gmail.com>
2023-06-29 12:56:51 -0300

    chore(web): review text

--- packages/web/src/components/Text/index.tsx
+++ packages/web/src/components/Text/index.tsx
@@ -1,56 +1,44 @@
 import { ComponentVariants, TypeGuards, useDefaultComponentStyle, useI18N } from '@codeleap/common'
-import { ComponentPropsWithoutRef, ElementType } from 'react'
+import React, { ComponentPropsWithoutRef, ElementType } from 'react'
 import { StylesOf } from '../../types/utility'
 import { TextComposition, TextPresets } from './styles'
 
 export * from './styles'
 
-export type TextProps<T extends ElementType> = {
-  component?: T
-  text: string
-  styles?: StylesOf<TextComposition>
-  debugName?: string
-} & ComponentPropsWithoutRef<T> &
-  ComponentVariants<typeof TextPresets>
-
-const getTextContent = (text: TextProps<any>['text']) => {
-  const { t } = useI18N()
-
-  let content = ''
-
-  if (TypeGuards.isArray(text)) {
-    text.forEach((message, i) => {
-      const space = (i !== 0 ? ' ' : '')
-
-      try {
-        const translatedMessage = t(String(message))
-        content = content + space + translatedMessage
-      } catch {
-        content = content + space + message
-      }
-    })
-  } else {
-    try {
-      content = t(String(text))
-    } catch (err) {
-      content = text
-    }
+export type TextProps<T extends ElementType> =
+  ComponentPropsWithoutRef<T> &
+  ComponentVariants<typeof TextPresets> & {
+    component?: T
+    text: string
+    styles?: StylesOf<TextComposition>
+    debugName?: string
+    msg?: string
   }
 
-  return content
+const defaultProps: Partial<TextProps<'p'>> = {
+  debugName: 'Text component',
+  component: 'p',
 }
 
 export const Text = <T extends ElementType>(textProps: TextProps<T>) => {
+  const allProps = {
+    ...Text.defaultProps,
+    ...textProps,
+  }
+
   const {
     variants = [],
     responsiveVariants = {},
+    styles = {},
     text = null,
     children,
-    component = 'p',
-    styles,
-    debugName = 'Text component',
+    component: Component,
+    debugName,
+    msg = null,
     ...props
-  } = textProps
+  } = allProps
+
+  const { t } = useI18N()
 
   const variantStyles = useDefaultComponentStyle<'u:Text', typeof TextPresets>('u:Text', {
     variants,
@@ -59,9 +47,15 @@ export const Text = <T extends ElementType>(textProps: TextProps<T>) => {
     rootElement: 'text',
   })
 
-  let content = getTextContent(text)
+  let content = ''
 
-  const Component = component
+  const _text = TypeGuards.isString(msg) ? msg : text
+
+  try {
+    content = t(String(_text))
+  } catch (err) {
+    content = text
+  }
 
   return (
     <Component
@@ -72,3 +66,5 @@ export const Text = <T extends ElementType>(textProps: TextProps<T>) => {
     </Component>
   )
 }
+
+Text.defaultProps = defaultProps
