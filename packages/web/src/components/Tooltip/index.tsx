796b83e36367943c02ddf94cce58c4a760e3e9e9
Luiz Felipe <felipefelizatti215@gmail.com>
2023-06-29 03:19:06 -0300

    chore(web): improvement tooltip props & hover events

--- packages/web/src/components/Tooltip/index.tsx
+++ packages/web/src/components/Tooltip/index.tsx
@@ -7,7 +7,6 @@ import {
   Content as TooltipContent,
   Arrow as TooltipArrow,
   TooltipProps as PrimitiveTooltipProps,
-
   TooltipContentProps,
   TooltipPortalProps,
   TooltipArrowProps,
@@ -28,21 +27,39 @@ type TooltipComponentProps = {
 }
 
 export type TooltipProps = PrimitiveTooltipProps & TooltipComponentProps & {
+  toggle?: AnyFunction
+  visible?: boolean
   content: React.ReactNode | ((props: TooltipProps) => JSX.Element)
   styles?: StylesOf<TooltipComposition>
   side?: 'left' | 'right' | 'bottom' | 'top'
   openOnPress?: boolean
   openOnHover?: boolean
-
   disabled?: boolean
   delayDuration?: number
-
   onOpen?: AnyFunction
   onClose?: AnyFunction
+  onValueChange?: (value: boolean) => void
+  onHover?: (value: boolean) => void
+  onPress?: (value: boolean) => void
 } & ComponentVariants<typeof TooltipPresets>
 
+const defaultProps: Partial<TooltipProps> = {
+  openOnPress: true,
+  openOnHover: true,
+  disabled: false,
+  delayDuration: 0,
+  side: 'bottom',
+}
+
 export const Tooltip: ComponentWithDefaultProps<TooltipProps> = (props: TooltipProps) => {
+  const allProps = {
+    ...Tooltip.defaultProps,
+    ...props,
+  }
+
   const {
+    toggle: _toggle = null,
+    visible: _visible = null,
     children,
     content: Content,
     side,
@@ -52,20 +69,19 @@ export const Tooltip: ComponentWithDefaultProps<TooltipProps> = (props: TooltipP
     openOnHover,
     onOpen,
     onClose,
-
-    contentProps,
-    portalProps,
-    arrowProps,
-    triggerProps,
-    providerProps,
-    variants,
-    responsiveVariants,
-    styles,
+    onValueChange,
+    onHover,
+    onPress,
+    contentProps = {},
+    portalProps = {},
+    arrowProps = {},
+    triggerProps = {},
+    providerProps = {},
+    variants = [],
+    responsiveVariants = {},
+    styles = {},
     ...rest
-  } = {
-    ...Tooltip.defaultProps,
-    ...props,
-  }
+  } = allProps
 
   const variantsStyles = useDefaultComponentStyle<'u:Tooltip', typeof TooltipPresets>('u:Tooltip', {
     responsiveVariants,
@@ -73,50 +89,71 @@ export const Tooltip: ComponentWithDefaultProps<TooltipProps> = (props: TooltipP
     styles,
   })
 
-  const [visible, toggle] = useState(false)
-  const isNotVisible = !visible
-  const notOpenOnPress = !openOnPress
-  const notOpenOnHover = !openOnHover
+  const hasStateProps = !TypeGuards.isNil(_visible) && TypeGuards.isFunction(_toggle)
 
-  let open = isNotVisible ? undefined : true
+  const [visible, toggle] = hasStateProps ? [_visible, _toggle] : useState(false)
 
-  const tooltipDirectionStyle = side ? variantsStyles[`wrapper:${side}`] : variantsStyles.wrapper
+  const tooltipDirectionStyle = React.useMemo(() => {
+    return side ? variantsStyles[`wrapper:${side}`] : variantsStyles.wrapper
+  }, [side, variantsStyles])
 
-  if (disabled) {
-    open = false
-  }
-
-  if (notOpenOnHover) {
-    open = visible
-  }
-
-  function handleIsTooltipOpen() {
-    if (disabled || notOpenOnPress) return
-
-    toggle((prev) => !prev)
-  }
-
-  function handleOpenState(_open:boolean) {
-    if (_open === true) {
+  function handleToggle(_value: boolean, isToggle = true) {
+    if (_value === true) {
       onOpen?.()
     } else {
       onClose?.()
     }
+
+    if (isToggle) toggle(_value)
+
+    if (TypeGuards.isFunction(onValueChange)) onValueChange?.(_value)
   }
 
-  const onOpenChange = notOpenOnHover ? () => null : handleOpenState
+  function onOpenChange(_open: boolean) {
+    handleToggle(_open, false)
+  }
+
+  const _onHover = (type: 'enter' | 'leave') => {
+    if (!openOnHover) return
+
+    const value = !visible
+
+    if (type === 'leave' && visible === false) return
+
+    handleToggle(value)
+    onHover?.(value)
+  }
+  
+  const _onPress = () => {
+    if (!openOnPress) return
+
+    const value = !visible
+
+    handleToggle(value)
+    onPress?.(value)
+  }
 
   return (
     <TooltipContainer {...providerProps}>
-      <TooltipWrapper delayDuration={delayDuration} open={open} onOpenChange={onOpenChange}
-        {...rest}>
-        <TooltipTrigger onClick={handleIsTooltipOpen} asChild {...triggerProps}>
+      <TooltipWrapper
+        delayDuration={delayDuration} 
+        open={visible} 
+        onOpenChange={onOpenChange}
+        {...rest}
+      >
+        <TooltipTrigger 
+          onClick={_onPress}
+          onMouseEnter={() => _onHover('enter')}
+          onMouseLeave={() => _onHover('leave')}
+          asChild
+          {...triggerProps}
+        >
           {children}
         </TooltipTrigger>
-        <TooltipPortal {...portalProps} >
+        <TooltipPortal {...portalProps}>
           <TooltipContent css={[tooltipDirectionStyle, variantsStyles.wrapper]} sideOffset={2} side={side} {...contentProps}>
             {
-              TypeGuards.isFunction(Content) ? <Content {...props} /> : Content
+              TypeGuards.isFunction(Content) ? <Content {...allProps} /> : Content
             }
             <TooltipArrow {...arrowProps} />
           </TooltipContent>
@@ -127,12 +164,6 @@ export const Tooltip: ComponentWithDefaultProps<TooltipProps> = (props: TooltipP
   )
 }
 
-Tooltip.defaultProps = {
-  openOnPress: true,
-  openOnHover: true,
-  disabled: false,
-  delayDuration: 0,
-  side: 'bottom',
-}
+Tooltip.defaultProps = defaultProps
 
 export * from './styles'
