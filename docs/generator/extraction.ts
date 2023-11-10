c9c8ac9817de07552eebc40bedae8af8f2eec7ad
Luiz Felipe <felipefelizatti215@gmail.com>
2023-11-10 12:47:49 -0300

    chore(docs): extractionIntersection

---
+++ docs/generator/extraction.ts
@@ -0,0 +1,10 @@
+import * as TypedocJSON from '../typedocs/web/test_components/Badge.json'
+
+type DocType = typeof TypedocJSON
+
+function extractionIntersection(part: DocType['children'][0]['type']) {
+  
+  // if () {
+
+  // }
+}
