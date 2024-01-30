4e9bbf1ae0b95809b326e8dcdbc4c7e6e634b8b1
Luiz Felipe <felipefelizatti215@gmail.com>
2024-01-30 10:28:13 -0300

    chore(mobile): add useQueryListEffect

---
+++ packages/mobile/src/utils/crud.ts
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useFocusEffect } from '@react-navigation/native'
+import { AnyFunction, TypeGuards, UseListEffect } from '@codeleap/common'
+
+type useQueryListEffect = (
+  listQuery: Parameters<UseListEffect>[0],
+  options?: {
+    staleTime?: number
+    initialStale?: boolean
+    cancelQueryEnabled?: boolean
+    refreshQueryEnabled?: boolean
+    onFocus?: AnyFunction
+    onBlur?: AnyFunction
+  }
+) => void
+
+export const useQueryListEffect: useQueryListEffect = (listQuery, options = {}) => {
+  const {
+    staleTime = 10000,
+    initialStale = listQuery?.query?.isStale,
+    cancelQueryEnabled = true,
+    refreshQueryEnabled = true,
+    onFocus,
+    onBlur,
+  } = options
+
+  const staleRefetch = React.useRef(initialStale)
+  const staleTimeout = React.useRef(null)
+
+  useFocusEffect(
+    React.useCallback(() => {
+      if (staleRefetch.current && refreshQueryEnabled) {
+        listQuery?.refreshQuery()
+      }
+
+      if (TypeGuards.isFunction(onFocus)) {
+        onFocus?.()
+      }
+
+      return () => {
+        if (cancelQueryEnabled) listQuery?.cancelQuery?.()
+        staleRefetch.current = false
+
+        if (staleTimeout.current == null) {
+          staleTimeout.current = setTimeout(() => {
+            staleRefetch.current = true
+            clearTimeout(staleTimeout.current)
+            staleTimeout.current = null
+          }, staleTime)
+        }
+
+        if (TypeGuards.isFunction(onBlur)) {
+          onBlur?.()
+        }
+      }
+    }, [staleRefetch.current])
+  )
+}
