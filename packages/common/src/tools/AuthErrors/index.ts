c9fa23f8c29d845e16a510a523864ffc7ece805d
Luiz Felipe <felipefelizatti215@gmail.com>
2024-02-02 10:52:57 -0300

    chore(common): AppAuthErrors

---
+++ packages/common/src/tools/AuthErrors/index.ts
@@ -0,0 +1,116 @@
+import { DEFAULT_AUTH_ERRORS } from './data'
+import { DefaultAuthError, Err, TAuthError } from './types'
+
+export * from './types'
+
+class AuthError extends Error {
+  code: TAuthError['code']
+  msg: TAuthError['msg']
+
+  constructor(message: TAuthError['msg'], code: TAuthError['code']) {
+    const _error = 'AuthError:' + code?.toLocaleUpperCase()
+    super(_error)
+    this.name = _error
+    this.code = code
+    this.msg = message
+  }
+}
+
+type ErrorHandler = (err: AuthError | null, module: string) => void
+
+export class AppAuthErrors<T extends Record<string, string | object>, F extends ErrorHandler> {
+  public errors: Omit<Record<keyof T | keyof typeof DEFAULT_AUTH_ERRORS, AuthError>, 'social'>
+
+  public social: Omit<Record<keyof T['social'] | keyof typeof DEFAULT_AUTH_ERRORS['social'], AuthError>, 'social'>
+
+  public onError: (err: Err, module?: string) => void
+
+  public defaultErrors = DEFAULT_AUTH_ERRORS
+
+  private socialKey: string = 'social'
+  
+  constructor(newErrors: T, errorFunction: F) {
+    const defaultErrors = DEFAULT_AUTH_ERRORS
+    
+    const socialAuthErrors = {
+      ...defaultErrors[this.socialKey],
+      ...(newErrors?.[this.socialKey] as object ?? {}),
+    } as any
+
+    const socialErrors = this.registryErrors(socialAuthErrors)
+    this.social = socialErrors
+
+    if (!!newErrors['social']) {
+      delete newErrors['social']
+    }
+
+    if (!!defaultErrors['social']) {
+      delete defaultErrors['social']
+    }
+
+    const baseAuthErrors = {
+      ...defaultErrors,
+      ...(newErrors as object ?? {}),
+    } as any
+
+    const authErrors = this.registryErrors(baseAuthErrors)
+    this.errors = authErrors
+
+    this.registryErrorFunction(errorFunction)
+  }
+
+  public getError(err: Err): AuthError | null {
+    if (!err) return null
+
+    if (typeof err == 'string') {
+      return this.errors?.[err] ?? this.social?.[err]
+    }
+
+    const authError = err as TAuthError
+
+    if (!!authError?.code) {
+      return this.errors?.[authError?.code] ?? this.social?.[authError?.code]
+    }
+
+    return null
+  }
+
+  public isError(err: Err, key: Exclude<keyof T | DefaultAuthError, 'social'>) {
+    const _error = this.errors?.[key]
+
+    return err == _error.code || (err as TAuthError)?.code == _error.code
+  }
+
+  public verifyError(err: Err): boolean {
+    return !!(this.getError(err))
+  }
+
+  public exception(err: Err): void | AuthError {
+    const _error = this.getError(err)
+
+    if (_error != null) {
+      throw new AuthError(_error?.msg, _error?.code)
+    }
+  }
+
+  private registryErrors(unregisteredErrors: T): any {
+    const state = {}
+
+    for (const errorKey in (unregisteredErrors as object)) {
+      const errorMsg = unregisteredErrors[errorKey] as string
+
+      if (typeof errorMsg === 'string' || errorMsg == null) {
+        state[errorKey] = new AuthError(errorMsg, errorKey)
+      }
+    }
+
+    return state
+  }
+
+  private registryErrorFunction(pureErrorFunction: Function) {
+    this.onError = (err, module = null) => {
+      const authError = this.getError(err)
+      pureErrorFunction?.(authError, module)
+    }
+  }
+}
---
+++ packages/common/src/tools/AuthErrors/types.ts
@@ -0,0 +1,10 @@
+import { DEFAULT_AUTH_ERRORS } from './data'
+
+export type TAuthError = {
+  code: string
+  msg: string | null
+}
+
+export type Err = TAuthError | null | string
+
+export type DefaultAuthError = keyof typeof DEFAULT_AUTH_ERRORS
