170bd9ada541f359af54289d3bb378db69665149
Luiz Felipe <felipefelizatti215@gmail.com>
2024-02-08 10:24:16 -0300

    chore(mobile): rn 0.73.4

--- syncpack.config.js
+++ syncpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
       "label": "React native",
       "dependencies": ["react-native"],
       packages: ["**"],
-      pinVersion: "0.73.3"
+      pinVersion: "0.73.4"
 
     },
     {
