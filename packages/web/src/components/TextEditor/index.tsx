a5fae7cb1316f8dd73e6e0adf2e2f0e5623c06fa
Takkuya <takuyagabriel@gmail.com>
2024-02-08 22:24:59 -0300

    chore(web-template): textEditor disabled and error states

--- packages/web/src/components/TextEditor/index.tsx
+++ packages/web/src/components/TextEditor/index.tsx
@@ -1,9 +1,9 @@
-import { useCallback, useDefaultComponentStyle } from '@codeleap/common'
+import React from 'react'
+import { onUpdate, useCallback, useDefaultComponentStyle, useState, useValidate } from '@codeleap/common'
 import { BubbleMenu, FloatingMenu, EditorContent } from '@tiptap/react'
-import { FileInput, View } from '../components'
+import { FileInput, Text, View } from '../components'
 import { TextEditorProps } from './types'
 import { TextEditorPresets } from './styles'
-
 export * from './styles'
 export * from './types'
 
@@ -19,12 +19,42 @@ export const TextEditor = (props: TextEditorProps) => {
     floatingMenuProps,
     toolbarComponent,
     fileInputRef,
+    _error,
+    validate,
   } = props
+
+  const [_isFocused, setIsFocused] = useState(false)
+  const validation = useValidate(editor?.getText() ?? '', validate)
+
+  const hasError = !validation.isValid || _error
+  const errorMessage = validation.message || _error
+  const isDisabled = !editor?.isEditable ?? null
+
   const variantStyles = useDefaultComponentStyle<'u:TextEditor', typeof TextEditorPresets>('u:TextEditor', {
     variants,
     styles,
     responsiveVariants,
   })
+
+  const handleBlur = React.useCallback(() => {
+    validation?.onInputBlurred()
+    setIsFocused(false)
+  }, [validation?.onInputBlurred])
+
+  const handleFocus = React.useCallback(() => {
+    validation?.onInputFocused()
+    setIsFocused(true)
+  }, [validation?.onInputFocused])
+
+  onUpdate(() => {
+    editor?.on('blur', handleBlur)
+    editor?.on('focus', handleFocus)
+
+    return () => {
+      editor?.off('blur', handleBlur)
+      editor?.off('focus', handleFocus)
+    }
+  }, [editor, handleBlur, handleFocus])
 
   const _BubbleMenu = useCallback(() => {
     return (
@@ -46,14 +76,30 @@ export const TextEditor = (props: TextEditorProps) => {
     )
   }, [editor])
 
+  const editorStyles = [
+    variantStyles.editor,
+    hasError && variantStyles['editor:error'],
+    isDisabled && variantStyles['editor:disabled'],
+  ]
+
   if (!editor) return null
   return (
-    <View style={{ ...variantStyles.wrapper, ...style }}>
+    <View
+      css={[
+        variantStyles.wrapper,
+        hasError && variantStyles['wrapper:error'],
+        {
+          '.tiptap': editorStyles,
+        },
+        style,
+      ]}
+    >
       {toolbarComponent}
       {children}
-      <_BubbleMenu/>
-      <_FloatingMenu/>
-      <EditorContent editor={editor}/>
+      <_BubbleMenu />
+      <_FloatingMenu />
+      <EditorContent editor={editor} />
+      {hasError ? <Text text={errorMessage} css={variantStyles['errorMessage:error']} /> : null}
       <FileInput
         ref={fileInputRef}
       />
