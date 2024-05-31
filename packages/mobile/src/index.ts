e35eec41731e790cf6f8aa8ce05d6a3517254913
Luiz Felipe <felipefelizatti215@gmail.com>
2024-05-31 17:07:16 -0300

    fix

--- packages/mobile/src/index.ts
+++ packages/mobile/src/index.ts
@@ -1,6 +1,7 @@
 export * from './components/components'
 export { Linking } from 'react-native'
 export * from './utils'
+export * from './hooks'
 import uuid from 'react-native-uuid'
 import * as RNKeyboardAwareScrollView from './utils/KeyboardAware'
 export { uuid, RNKeyboardAwareScrollView }
