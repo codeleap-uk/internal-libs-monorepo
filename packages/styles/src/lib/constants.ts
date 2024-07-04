767ff94af0e113ac9c69b7ef9eb38289a0e8beaa
Luiz Felipe <felipefelizatti215@gmail.com>
2024-07-04 11:40:43 -0300

    disable styles cache

--- packages/styles/src/lib/constants.ts
+++ packages/styles/src/lib/constants.ts
@@ -1,6 +1,6 @@
 
 export const STORES_PERSIST_VERSION = 47
 
-export const STORE_CACHE_ENABLED = true
+export const STORE_CACHE_ENABLED = false
 
-export const CACHE_ENABLED = true
+export const CACHE_ENABLED = false
