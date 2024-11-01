77224299d068e19d4d7d237857f04b330aa526ce
PSouza-10 <paulosouza300272@gmail.com>
2024-11-01 16:53:33 -0300

    bun, fix build, and remove docs

--- apps/docs/src/app/logger.ts
+++
@@ -1,7 +0,0 @@
-import { Logger, LoggerTypes } from '@codeleap/common'
-import { Settings } from './Settings'
-
-export const logger = new Logger(Settings, [], null)
-
-// @ts-ignore
-global.logger = logger
