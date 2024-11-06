0ec2b7cf58d55653730754ad2a1dde2ea02b5871
PSouza-10 <paulosouza300272@gmail.com>
2024-11-06 02:04:51 -0300

    try to publish with bun

--- publish.sh
+++ publish.sh
@@ -8,9 +8,4 @@ else
   TAG="rc-latest"
 fi
 
-for i in 'common' 'mobile' 'web' 'config' 'cli' 'styles'
-do
-cd packages/$i
-npm publish --tag $TAG
-cd ../..
-done
+bunx @morlay/bunpublish --tag $TAG
