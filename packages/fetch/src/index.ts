b2fdfe270c5a066e6e91429fbc7825a753e088ae
Luiz Felipe <felipefelizatti215@gmail.com>
2024-12-06 16:31:30 -0300

    fetch package

---
+++ packages/fetch/src/index.ts
@@ -0,0 +1,23 @@
+import { AppSettings } from '@codeleap/common'
+import { RequestClientConfig } from './types'
+import { RequestClient } from './RequestClient'
+import { AxiosHeaders } from 'axios'
+
+export * from 'axios'
+export * from './types'
+
+export { RequestClient } from './RequestClient'
+
+export function makeFetcher(
+  settings: AppSettings,
+  override?: Partial<RequestClientConfig>,
+) {
+  return new RequestClient({
+    baseURL: settings.Environment.IsDev ? settings.Fetch.DevelopmentURL : settings.Fetch.ProductionURL,
+    automaticMultipartParsing: true,
+    headers: new AxiosHeaders(),
+    ...override,
+  })
+}
+
+export { APIError } from './APIError'
