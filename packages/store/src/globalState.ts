e52bee24665b7f53a9b3b68d8b57045d1f3209e3
Luiz Felipe <felipefelizatti215@gmail.com>
2024-12-06 15:56:23 -0300

    new store package

---
+++ packages/store/src/globalState.ts
@@ -0,0 +1,45 @@
+import { useStore } from '@nanostores/react'
+import { setPersistentEngine, persistentAtom } from '@nanostores/persistent'
+import { atom } from 'nanostores'
+import { GlobalState, GlobalStateConfig, StateSelector } from './types'
+import { stateAssign, useStateSelector } from './utils'
+
+const defaultConfig: GlobalStateConfig = {
+  persist: false,
+  persistKey: null,
+}
+
+export const setGlobalStatePersistor = setPersistentEngine
+
+export function globalState<T>(value: T, config: GlobalStateConfig = defaultConfig): GlobalState<T> {
+  const { persistKey, persist } = config
+
+  if (persist && !persistKey) {
+    throw new Error('Atom persist invalid configuration: "persistKey" is required when "persist" is enabled.')
+  }
+
+  const store = persist ? persistentAtom<T>(persistKey, value, {
+    encode: JSON.stringify,
+    decode: JSON.parse,
+  }) : atom(value)
+
+  return new Proxy(store, {
+    get(target, prop, receiver) {
+      if (prop === 'use') {
+        return (selector?: StateSelector<T, any>) => {
+          if (!selector) return useStore(target)
+          return useStateSelector(target, selector)
+        }
+      }
+
+      if (prop === 'set') {
+        return (newValue: Partial<T>) => {
+          const value = stateAssign(newValue, target.get())
+          target.set(value)
+        }
+      }
+
+      return Reflect.get(target, prop, receiver)
+    }
+  }) as unknown as GlobalState<T>
+}
---
+++ packages/store/src/types.ts
@@ -0,0 +1,20 @@
+import { WritableAtom } from 'nanostores'
+import { PersistentStore, PersistentEvents, PersistentEvent } from '@nanostores/persistent'
+
+export type StateSelector<S, R> = (state: S) => R
+
+export type GlobalState<T> = Omit<WritableAtom<T>, 'set'> & {
+  use: <Selected = T>(selector?: StateSelector<T, Selected>) => Selected
+  set: (newValue: Partial<T>) => T
+}
+
+export type GlobalStateConfig = {
+  persist?: boolean
+  persistKey?: string
+}
+
+export type GlobalStatePersistor = PersistentStore
+
+export type GlobalStatePersistorEvents = PersistentEvents
+
+export type GlobalStatePersistorEvent = PersistentEvent
