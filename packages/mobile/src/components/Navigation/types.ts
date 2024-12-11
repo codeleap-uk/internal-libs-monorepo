c994f51bc565d4e7edb37386fdcf2a43db49b922
Luiz Felipe <felipefelizatti215@gmail.com>
2024-12-11 18:29:45 -0300

    remove navigation

--- packages/mobile/src/components/Navigation/types.ts
+++ packages/mobile/src/components/Navigation/types.ts
@@ -1,28 +1,30 @@
-import { AppIcon } from '@codeleap/styles'
-import { Navigators } from './constants'
+// import { AppIcon } from '@codeleap/styles'
+// import { Navigators } from './constants'
 
-export type TNavigators = typeof Navigators
-export type NavigatorType = keyof TNavigators
+// export type TNavigators = typeof Navigators
+// export type NavigatorType = keyof TNavigators
 
-export type PropTypes = {
-    [P in NavigatorType] : {
-        Screen: Omit<React.ComponentPropsWithRef<TNavigators[P]['Screen']>, 'children'|'name'>
-        Navigator: Omit<React.ComponentPropsWithRef<TNavigators[P]['Navigator']>, 'children'|'name'>
-        Group: Omit<React.ComponentPropsWithRef<TNavigators[P]['Group']>, 'children'|'name'>
-    }
-}
-export type SceneComponent<K extends NavigatorType> = PropTypes[K]['Screen']
-export type SceneOptions<K extends NavigatorType> =
-    { icon?: AppIcon; default?:SceneComponent<K> } & PropTypes[K]['Screen']
-export type Scene<K extends NavigatorType> = SceneComponent<K> | SceneOptions<K>
+// export type PropTypes = {
+//     [P in NavigatorType] : {
+//         Screen: Omit<React.ComponentPropsWithRef<TNavigators[P]['Screen']>, 'children'|'name'>
+//         Navigator: Omit<React.ComponentPropsWithRef<TNavigators[P]['Navigator']>, 'children'|'name'>
+//         Group: Omit<React.ComponentPropsWithRef<TNavigators[P]['Group']>, 'children'|'name'>
+//     }
+// }
+// export type SceneComponent<K extends NavigatorType> = PropTypes[K]['Screen']
+// export type SceneOptions<K extends NavigatorType> =
+//     { icon?: AppIcon; default?:SceneComponent<K> } & PropTypes[K]['Screen']
+// export type Scene<K extends NavigatorType> = SceneComponent<K> | SceneOptions<K>
 
-export type Scenes<K extends NavigatorType> = {
-    [x:string] : Scene<K>
-}
+// export type Scenes<K extends NavigatorType> = {
+//     [x:string] : Scene<K>
+// }
 
-export type SceneProps = any
+// export type SceneProps = any
 
-export type NavigationProps<T extends NavigatorType> = {
-    type: T
-    scenes: Scenes<T>
-} & PropTypes[T]['Navigator']
+// export type NavigationProps<T extends NavigatorType> = {
+//     type: T
+//     scenes: Scenes<T>
+// } & PropTypes[T]['Navigator']
+
+export {}
--- packages/mobile/src/components/Navigation/utils.tsx
+++ packages/mobile/src/components/Navigation/utils.tsx
@@ -1,57 +1,58 @@
-import React from 'react'
-import { Navigation } from './Navigation'
+// import React from 'react'
+// import { Navigation } from './Navigation'
 
-export function createAppNavigation(Scenes:any) {
+// export function createAppNavigation(Scenes:any) {
 
-  const AllScenes = Object.entries<any>(Scenes).reduce((allScenes, [moduleName, content]) => {
+//   const AllScenes = Object.entries<any>(Scenes).reduce((allScenes, [moduleName, content]) => {
 
-    const subScenes = []
+//     const subScenes = []
 
-    for (const [name, sceneContent] of Object.entries(content.scenes)) {
-      subScenes.push(
-        [`${moduleName}.${name}`, sceneContent],
-      )
-    }
+//     for (const [name, sceneContent] of Object.entries(content.scenes)) {
+//       subScenes.push(
+//         [`${moduleName}.${name}`, sceneContent],
+//       )
+//     }
 
-    return [
-      ...allScenes,
-      ...subScenes,
-    ]
-  }, [])
+//     return [
+//       ...allScenes,
+//       ...subScenes,
+//     ]
+//   }, [])
 
-  const AppScenes = Object.fromEntries(AllScenes)
+//   const AppScenes = Object.fromEntries(AllScenes)
 
-  const SCENES_RESULT = Object.fromEntries(
-    Object.entries<any>(Scenes)
-      .map(([S, { _ig_scenes, exclude, type, navigationProps, ...otherProps }]) => {
-        let filterScenes = null
+//   const SCENES_RESULT = Object.fromEntries(
+//     Object.entries<any>(Scenes)
+//       .map(([S, { _ig_scenes, exclude, type, navigationProps, ...otherProps }]) => {
+//         let filterScenes = null
 
-        if (exclude) {
-          filterScenes = Object.fromEntries(
-            AllScenes.filter(([path]) => !exclude.some(param => path.startsWith(param))),
-          )
-        }
+//         if (exclude) {
+//           filterScenes = Object.fromEntries(
+//             AllScenes.filter(([path]) => !exclude.some(param => path.startsWith(param))),
+//           )
+//         }
 
-        const Component = () => <Navigation
-          type={type || 'Stack'}
-          scenes={filterScenes || AppScenes}
-          screenOptions={{
-            headerShown: false,
-            ...navigationProps?.screenOptions,
-          }}
-          {...navigationProps}
-        />
-        // console.log('SCENES_RESULT create', { _ig_scenes, exclude, type, navigationProps, otherProps })
+//         const Component = () => <Navigation
+//           type={type || 'Stack'}
+//           scenes={filterScenes || AppScenes}
+//           screenOptions={{
+//             headerShown: false,
+//             ...navigationProps?.screenOptions,
+//           }}
+//           {...navigationProps}
+//         />
+//         // console.log('SCENES_RESULT create', { _ig_scenes, exclude, type, navigationProps, otherProps })
 
-        return [S, {
-          ...otherProps,
-          component: Component,
-        }]
-      }),
-  )
+//         return [S, {
+//           ...otherProps,
+//           component: Component,
+//         }]
+//       }),
+//   )
 
-  // console.log('SCENES_RESULT', SCENES_RESULT)
+//   // console.log('SCENES_RESULT', SCENES_RESULT)
 
-  return SCENES_RESULT
-}
+//   return SCENES_RESULT
+// }
 
+export {}
