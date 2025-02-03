a496abaf4151e945854a7206075da6f9042cf24a
Luiz Felipe <felipefelizatti215@gmail.com>
2025-02-03 15:58:10 -0300

    memo docs, add staticNavigationExtractOptions, remove Navigation

--- packages/mobile/src/modules/reactNavigation.ts
+++ packages/mobile/src/modules/reactNavigation.ts
@@ -1,14 +1,64 @@
-import React from 'react'
-export { createStackNavigator } from '@react-navigation/stack'
-export { createDrawerNavigator } from '@react-navigation/drawer'
-export { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { NavigationContainerRefContext, NavigationContext } from '@react-navigation/core'
-export { useIsFocused } from '@react-navigation/native'
+import { AnyRecord } from '@codeleap/types'
+import { deepMerge } from '@codeleap/utils'
+import { createPathConfigForStaticNavigation } from '@react-navigation/native'
 
-export function useNavigationContext() {
-  const root = React.useContext(NavigationContainerRefContext)
-  const navigation = React.useContext(NavigationContext)
-  if (!navigation && !root) return undefined
+type TreeStack = Parameters<typeof createPathConfigForStaticNavigation>[0]
 
-  return (navigation ?? root)
+/**
+ * Extracts screen options from a static navigation tree, consolidating default, group, 
+ * and individual screen options into a single object. Supports nested navigators.
+ */
+export function staticNavigationExtractOptions(stack: TreeStack) {
+  const { config } = stack
+
+  // @ts-ignore
+  const defaultOptions = config.screenOptions
+
+  let result = {}
+
+  function extractScreens(screens: TreeStack['config'], groupKey: string | null, groupOptions: AnyRecord) {
+    let screenResults = {}
+
+    for (const screenKey in screens) {
+      const screen = screens[screenKey]
+
+      const key = groupKey ? `${groupKey}.${screenKey}` : screenKey
+
+      const isComponentInstance = typeof screen !== 'object'
+
+      const defaultScreenOptions = deepMerge(defaultOptions, groupOptions)
+
+      if (isComponentInstance) {
+        screenResults[key] = defaultScreenOptions
+        continue
+      }
+
+      const screenOptions = deepMerge(defaultScreenOptions, screen.options)
+
+      const isNavigationInstance = !!screen?.screen?.config?.screens
+
+      if (isNavigationInstance) {
+        screenResults = deepMerge(
+          screenResults,
+          extractScreens(screen?.screen?.config?.screens, null, screenOptions)
+        )
+        continue
+      }
+
+      screenResults[key] = screenOptions
+    }
+
+    return screenResults
+  }
+
+  for (const groupKey in config.groups) {
+    const group = config.groups[groupKey]
+
+    // @ts-ignore
+    const groupOptions = group.screenOptions
+
+    result = deepMerge(result, extractScreens(group.screens, null, groupOptions))
+  }
+
+  return result
 }
