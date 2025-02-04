7433fce3eff718cf8002fa6ea85450f5448b23e9
Luiz Felipe <felipefelizatti215@gmail.com>
2025-02-04 14:57:11 -0300

    fix getDeviceLocale

--- packages/mobile/src/utils/locale.ts
+++ packages/mobile/src/utils/locale.ts
@@ -1,4 +1,15 @@
+import { findBestLanguageTag, getLocales } from 'react-native-localize'
 
+export function getDeviceLocale(dictionary: Record<string, any>, defaultLocale: string) {
+  const deviceLocales = getLocales().map(locale => locale?.languageTag)
 
-export const getDeviceLocale = (): string => 'en-GB'
+  const dictionaryLocales = Object.keys(dictionary)
 
+  const possibleLocales = [...new Set([...dictionaryLocales, ...deviceLocales])]
+
+  const bestLocale = findBestLanguageTag(possibleLocales)
+
+  if (!bestLocale) return defaultLocale
+
+  return bestLocale?.languageTag
+}
