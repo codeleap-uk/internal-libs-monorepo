4c5ce59b722a3458ee1d083e1bb5709c2e7ce084
leonardo-weber <weberleonardo2002@gmail.com>
2025-02-04 07:43:14 -0300

    fix crash on modals context

--- packages/mobile/src/utils/ModalManager/context.tsx
+++ packages/mobile/src/utils/ModalManager/context.tsx
@@ -52,7 +52,7 @@ export function Provider({ children }) {
           visible: newVisible,
           props,
         },
-        ...Object.fromEntries(attached),
+        ...Object.fromEntries(attached ?? []),
       }
 
     })
