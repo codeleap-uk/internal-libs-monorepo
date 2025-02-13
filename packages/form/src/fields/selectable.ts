da4e2c9d61556c94bbb6fdf645cb00b758b9d36b
Luiz Felipe <felipefelizatti215@gmail.com>
2025-02-13 16:47:54 -0300

    selectable min/max

--- packages/form/src/fields/selectable.ts
+++ packages/form/src/fields/selectable.ts
@@ -10,15 +10,23 @@ export type SelectableValidator<V extends VALUE, R = any, Err = any> = Validator
 
 type SelectableFieldOptions<V extends VALUE, Validate extends SelectableValidator<V>> = FieldOptions<V, Validate> & {
   options: Options<V>
+  minItems?: number
+  maxItems?: number
 }
 
 export class SelectableField<V extends VALUE, Validate extends SelectableValidator<V>> extends Field<V, Validate> {
   _type = "SELECTABLE"
 
   constructor(options: SelectableFieldOptions<V, Validate>) {
+    const { minItems = 1, maxItems = options?.options?.length } = options
+    
+    const zScheme = z.string().or(z.number())
+
     super({
-      validate: zodValidator(z.string().or(z.number())) as unknown as Validate,
-      ...options
+      validate: zodValidator(z.array(zScheme).min(minItems).max(maxItems).or(zScheme)) as unknown as Validate,
+      minItems,
+      maxItems,
+      ...options,
     })
   }
 }
