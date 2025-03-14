204baacd4e7147e97d4eadd8554489f5349d136f
Luiz Felipe <felipefelizatti215@gmail.com>
2025-03-14 19:45:06 -0300

    remove old permissions

--- packages/mobile/src/utils/index.ts
+++ packages/mobile/src/utils/index.ts
@@ -3,15 +3,10 @@ export * from './hooks'
 export * from './misc'
 export * from './NotificationManager'
 export * from './ModalManager'
-import * as Permissions from './PermissionManager'
-
-export { Permissions }
 export * from './KeyboardAware'
 export * from './input'
 export * from './theme'
 export * from './locale'
 export * from './Subscription'
 export * from './useQueryListRefresh'
-
 export * from './StorageManager'
-export * from './MobilePermissionManager'
