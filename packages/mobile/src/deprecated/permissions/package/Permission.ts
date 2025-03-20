37ff4c946e12ab9abe2e3030cfe8cee8cd0ec807
Luiz Felipe <felipefelizatti215@gmail.com>
2025-03-20 11:27:01 -0300

    mobile add deprecated module

---
+++ packages/mobile/src/deprecated/permissions/package/Permission.ts
@@ -0,0 +1,79 @@
+import { AnyFunction } from '@codeleap/types'
+import * as PermissionTypes from './types'
+
+const SCOPE = 'Permissions'
+
+export class Permission implements PermissionTypes.IPermission {
+  status: PermissionTypes.PermissionStatus
+
+  shouldAsk: boolean
+
+  constructor(private actions: PermissionTypes.PermissionActions & { log?: AnyFunction }, public name = '') {
+    this.actions = actions
+    this.shouldAsk = true
+    this.status = 'pending'
+
+  }
+
+  async ask(): Promise<void> {
+    const newState = await this.actions.onAsk()
+    this.actions.log(`Request for permission ${this.name} returned`, newState, SCOPE)
+    this.status = newState
+
+    if (newState === 'blocked') {
+      this.shouldAsk = false
+    }
+  }
+
+  async check(options?: PermissionTypes.CheckOptions): Promise<void> {
+    const _options: PermissionTypes.CheckOptions = {
+      askOnPending: true,
+      askOnDenied: false,
+      ask: true,
+      ...options,
+    }
+
+    this.status = await this.actions.onCheck()
+    this.actions.log(`Check for permission ${this.name} returned`, this.status, SCOPE)
+    if (!_options.ask) return
+
+    switch (this.status) {
+      case 'denied':
+        if (_options.askOnDenied) {
+          await this.ask()
+        }
+        break
+      case 'pending':
+        if (_options.askOnPending) {
+          await this.ask()
+        }
+        break
+    }
+  }
+
+  get isGranted() {
+    return this.status === 'granted'
+  }
+
+  get isDenied() {
+    return this.status === 'denied'
+  }
+
+  get isPending() {
+    return this.status === 'pending'
+  }
+
+  get isBlocked() {
+    return this.status === 'blocked'
+  }
+
+  get isUnavailable() {
+    return this.status === 'unavailable'
+
+  }
+
+  get isLimited() {
+    return this.status === 'limited'
+
+  }
+}
