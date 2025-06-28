b6d08ed1b1a83b567f5a8f95d7f196a63f5be963
Felipe Felizatti <felipefelizatti215@gmail.com>
2025-06-28 02:18:12 -0300

    Update date.ts

--- packages/utils/src/date.ts
+++ packages/utils/src/date.ts
@@ -6,9 +6,9 @@ const removeTimezoneAndFormat = (date: any, format = 'YYYY-MM-DD') => {
   let normalizedDate = date
 
   if (date instanceof Date) {
-    normalizedDate = date.toISOString().split('T')[0]
+    normalizedDate = date.toISOString().split('T')[0] + 'T12:00:00'
   } else if (typeof date === 'string' && date.includes('T')) {
-    normalizedDate = date.split('T')[0]
+    normalizedDate = date.split('T')[0] + 'T12:00:00'
   }
 
   return dayjs(normalizedDate).startOf('day').format(format)
