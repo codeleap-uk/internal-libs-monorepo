b1a74ecac5c2d33c1157aa01c511a49e8cfda637
Felipe Felizatti <felipefelizatti215@gmail.com>
2025-09-11 00:22:21 -0300

    adjs

--- packages/store/src/tests/slice.spec.ts
+++ packages/store/src/tests/slice.spec.ts
@@ -1,48 +1,50 @@
-import { expect, test } from "bun:test"
-import {globalState} from '../globalState'
-import { createStateSlice } from "../utils"
+import { expect, test, describe } from 'bun:test'
+import { globalState } from '../globalState'
+import { createStateSlice } from '../utils'
 
-test("slice selects consistently", () => {
-  const store = globalState({
-    a: 1,
-    b: "Test"
+describe('slice', () => {
+  test('slice selects consistently', () => {
+    const store = globalState({
+      a: 1,
+      b: 'Test'
+    })
+
+    const slice = createStateSlice(store,
+      (v: any) => v.a
+    )
+
+    store.set({
+      a: 4
+    })
+
+    expect(slice.get()).toBe(4)
+
+    store.set({
+      a: 10
+    })
+
+    expect(slice.get()).toBe(10)
   })
 
-  const slice = createStateSlice(store, 
-    v => v.a
-  )
+  test('slice sets consistently', () => {
+    const store = globalState({
+      a: 1,
+      b: 'Test'
+    })
 
-  store.set({
-    a: 4
+    const slice = createStateSlice(store,
+      (v: any) => v.a,
+      v => ({ a: v })
+    )
+
+    slice.set(4)
+
+    expect(slice.get()).toBe(4)
+    expect(store.get().a).toBe(4)
+
+    slice.set(10)
+
+    expect(slice.get()).toBe(10)
+    expect(store.get().a).toBe(10)
   })
-
-  expect(slice.get()).toBe(4)
-
-  store.set({
-    a: 10
-  })
-
-  expect(slice.get()).toBe(10)
-})
-
-test("slice sets consistently", () => {
-  const store = globalState({
-    a: 1,
-    b: "Test"
-  })
-
-  const slice = createStateSlice(store, 
-    v => v.a,
-    v => ({ a: v })
-  )
-
-  slice.set(4)
-
-  expect(slice.get()).toBe(4)
-  expect(store.get().a).toBe(4)
-
-  slice.set(10)
-
-  expect(slice.get()).toBe(10)
-  expect(store.get().a).toBe(10)
 })
