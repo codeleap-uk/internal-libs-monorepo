423e94eca2977e0bfc3563ccb49dfb8ac113fe27
Felipe Felizatti <felipefelizatti215@gmail.com>
2025-10-24 19:15:30 -0300

    upgrade

--- packages/web/src/components/TextEditor/types.ts
+++ packages/web/src/components/TextEditor/types.ts
@@ -1,21 +1,21 @@
 import { RefObject } from 'react'
 import { AnyFunction } from '@codeleap/types'
 import { StyledProp } from '@codeleap/styles'
-import { BubbleMenuProps, Editor, FloatingMenuProps } from '@tiptap/react'
+// import { BubbleMenuProps, Editor, FloatingMenuProps } from '@tiptap/react'
 import { FileInputRef } from '../FileInput'
 import { TextEditorComposition } from './styles'
 import { Validator } from '@codeleap/form'
 
 export type TextEditorProps = React.PropsWithChildren<{
-  editor: Editor & {
+  editor: any & {
     getText: AnyFunction
     isEditable: boolean
     on: AnyFunction
     off: AnyFunction
   }
   style?: StyledProp<TextEditorComposition>
-  bubbleMenuProps?: BubbleMenuProps & { renderContent: React.ReactNode }
-  floatingMenuProps?: FloatingMenuProps & { renderContent: React.ReactNode }
+  bubbleMenuProps?: any & { renderContent: React.ReactNode }
+  floatingMenuProps?: any & { renderContent: React.ReactNode }
   toolbarComponent?: React.ReactElement
   fileInputRef?: RefObject<FileInputRef>
   validate?: Validator<string, any, any>
--- syncpack.config.js
+++ syncpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
       "label": "React native",
       "dependencies": ["react-native"],
       packages: ["**"],
-      pinVersion: "0.81.4"
+      pinVersion: "0.81.5"
 
     },
     {
