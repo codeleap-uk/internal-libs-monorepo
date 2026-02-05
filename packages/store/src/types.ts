ad142ebdb95e0bca789765c2c964096b664b492b
PSouza-10 <paulosouza300272@gmail.com>
2026-02-04 22:38:31 -0300

    feat(store): add callback setter to global state and improve test coverage

--- packages/store/src/types.ts
+++ packages/store/src/types.ts
@@ -3,10 +3,13 @@ import { PersistentStore, PersistentEvents, PersistentEvent } from '@nanostores/
 
 export type StateSelector<S, R> = (state: S) => R
 
+export type StateSetterFunction<In, Out = In> = (current: In) => Out
+export type StateSetter<TIn, TOut = TIn> = TOut | StateSetterFunction<TIn, TOut>
+
 export type GlobalState<T> = Omit<WritableAtom<T>, 'set' | 'get'> & {
   use: <Selected = T>(selector?: StateSelector<T, Selected>) => Selected
 
-  set: (newValue: T extends Record<string, any> ? Partial<T> : T) => void
+  set: (newValue: T extends Record<string, any> ? StateSetter<T, Partial<T>> : StateSetter<T>) => void
 
   get: <Selected = T>(selector?: StateSelector<T, Selected>) => Selected extends undefined ? T : Selected
 
